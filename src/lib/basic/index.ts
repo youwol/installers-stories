341b332f282aac1093ad619ddd22dc7cd1151eba
greinisch <greinisch@youwol.com>
2022-05-18 11:37:29 +0200

    🎉 first version

---
+++ src/lib/basic/index.ts
@@ -0,0 +1,41 @@
+import { Core, Explorer } from '@youwol/platform-essentials'
+
+export async function install(
+    installer: Core.Installer,
+): Promise<Core.Installer> {
+    return installer.with({
+        fromManifests: [
+            {
+                id: '@youwol/installers-stories.basic',
+                contextMenuActions: ({ node, explorer, assetsGtwClient }) => [
+                    {
+                        name: 'New Story',
+                        icon: 'fas fa-story',
+                        authorized: true,
+                        exe: async () => {
+                            explorer.newAsset({
+                                parentNode: node as Explorer.AnyFolderNode,
+                                pendingName: 'new story',
+                                kind: 'story',
+                                request: assetsGtwClient.stories.create$({
+                                    queryParameters: { folderId: node.id },
+                                    body: { title: 'new story' },
+                                }),
+                            })
+                        },
+                        applicable: () => node instanceof Explorer.FolderNode,
+                    },
+                ],
+                applications: ['@youwol/stories'],
+                applicationsData: {
+                    '@youwol/stories': {
+                        toolboxes: [
+                            '@youwol/grapes-basics',
+                            '@youwol/@youwol/grapes-text-editors',
+                        ],
+                    },
+                },
+            },
+        ],
+    })
+}
